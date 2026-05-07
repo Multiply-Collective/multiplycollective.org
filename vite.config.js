b5872da4b8a6bf8e6245e13e09e04a0fd3ce97f0
Michael Parenti <mparenti677@gmail.com>
2026-05-06 21:59:15 -0500

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/multiplycollective.org/",
+  base: "/",
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
